edd823dddf9aaf6bb6b700f822ef73dea454d226
Veivneorul <64707127+TheGuardianLight@users.noreply.github.com>
2024-01-12 09:07:50 +0100

    Add files via upload

---
+++ netbox.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Vérifie si le script est lancé avec les permissions administrateurs
+echo "Vérification des permissions administrateurs..."
+if [ $EUID -ne 0 ]; then
+    echo "Vous devez lancer ce script avec les permissions administrateurs"
+    exit 1
+    else echo "Vous avez les permissions administrateurs"
+fi
+
+# Mise à jour des paquets et installation des dépendances
+echo "Mise à jour des paquets puis installation des dépendances..."
+apt update && apt upgrade -y
